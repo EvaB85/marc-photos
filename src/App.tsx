392663d942c5a2404b70ab360a036251fedd0d88
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-05 11:45:33 -0800

    added new files from pull

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.min.css";
 import { GoogleFont, TypographyStyle } from "react-typography";
 import typography from "../src/utils/typography";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import { Topbar } from "./components/Topbar";
@@ -13,15 +12,6 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App">
-          <Helmet>
-            <meta charSet="utf-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <meta name="theme-color" content="#000000" />
-            <title>Marc Schultz Photography</title>
-          </Helmet>
           <TypographyStyle typography={typography} />
           <GoogleFont typography={typography} />
 
